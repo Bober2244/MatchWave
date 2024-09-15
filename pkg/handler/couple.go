37b74065f38b722dddc66583771976688e86a382
Nikita Sergeev <59967001+NikitaSergeev07@users.noreply.github.com>
2024-09-16 01:54:25 +0300

    Базовая структура проекта (#6)

---
+++ pkg/handler/couple.go
@@ -0,0 +1,13 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) createCouple(c *gin.Context) {}
+
+func (h *Handler) getAllCouples(c *gin.Context) {}
+
+func (h *Handler) getCoupleById(c *gin.Context) {}
+
+func (h *Handler) updateCouple(c *gin.Context) {}
+
+func (h *Handler) deleteCouple(c *gin.Context) {}
