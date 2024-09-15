cf13f6afb6966085003f8d2faff58817f95642e3
bi_ba <sarutunan082@gmail.com>
2024-09-16 00:42:27 +0300

    Local changes

--- features/auth/src/main/java/dev/bober/auth/di/AuthModule.kt
+++ features/auth/src/main/java/dev/bober/auth/di/AuthModule.kt
@@ -1,7 +1,13 @@
 package dev.bober.auth.di
 
+import dev.bober.auth.authstart.AuthScreenViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val authModule = module {
+    //TODO: viewModel { AuthScreenViewModel() }
 
+    single {
+
+    }
 }
---
+++ features/auth/src/main/java/dev/bober/auth/forgotpassword/ForgotPasswordScreen.kt
@@ -0,0 +1,11 @@
+package dev.bober.auth.forgotpassword
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ForgotPasswordScreen(
+    modifier: Modifier = Modifier,
+) {
+    
+}
